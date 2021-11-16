c45eef95a71ba74d996dc3ace3c94da3a25378f8
Maneesh Singh <mann.biher@yahoo.co.in>
2021-11-15 21:23:44 -0500

    autoapprove distroy script

--- infrastructure/destroy.sh
+++ infrastructure/destroy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd irsa
 terraform init
-terraform destroy -input=false
+terraform destroy -input=false -auto-approve
 
 cd ../argocd
 terraform init
-terraform destroy -input=false
+terraform destroy -input=false -auto-approve
 
 cd ../eks
 terraform init
-terraform destroy -input=false
+terraform destroy -input=false -auto-approve
 
 cd ../ec2
 terraform init
-terraform destroy -input=false
+terraform destroy -input=false -auto-approve
 
 
 
